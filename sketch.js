9a56e6134ba80a09f6faf9c6ebdf73a05c19b2ff
Paarth-cool <68222769+Paarth-cool@users.noreply.github.com>
2020-09-15 20:29:37 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,79 +1,36 @@
-var ball;
-var position
-var database
-function setup(){
-    database = firebase.database()
-    createCanvas(500,500);
-    
-    ball.shapeColor = "red";
-    var ballposition = database.ref('ball/position')
-    ballposition.on("value",readposition,showerror)
-}
 
-function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
-}
-
-function writePosition(x,y){
-    database.ref('ball/position').set({
-        x:position.x+x,
-        y:position.y+y,        
-    })
-    
-}
-function showerror(){
-    console.log("errorreadingfromthedatabase")
-}
-function readposition(data){
-    position = data.val()
-    ball.x = position.x
-    ball.y = position.y
-}
 //Create variables here
 var dog
+var dogImage
 var dogfood
 var Foodcount
+var score = 30
 
 function preload()
 {
-  dog.Image = loadImage("images/Dog.png");
+  dogImage = loadImage("images/Dog.png");
   
 }
 
 function setup() {
     createCanvas(500, 500);
     dog = createSprite(250,250,10,10);
+    dog.addImage(dogImage)
+    dog.scale = 0.2
+    
 }
 
 
 function draw() {  
- 
+  background("lightgreen")
+  textSize(30)
+  text("food "+score,20,40)
   drawSprites();
-  //add styles here
-  if(keyDown(LEFT_ARROW)){
-  writePosition(-1,0);
+  if(keyDown(UP_ARROW)){
+      score = score-1
   }
-  else if(keyDown(RIGHT_ARROW)){
-  writePosition(1,0);
-  }
-  else if(keyDown(UP_ARROW)){
-  writePosition(0,-5);
-  }
-  else if(keyDown(DOWN_ARROW)){
-  writePosition(0,+1);
-  }
+ 
+
+  
   dog.display();
 }
